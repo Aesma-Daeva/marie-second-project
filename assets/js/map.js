5b4e6fdcb7992a61acfc1684ccd187066c2b13d1
Marie O <mariechris.obradovic@gmail.com>
2019-01-03 17:18:11 +0000

    Fix order of scripts
    Add checks for custom icon and map content

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,17 +1,44 @@
 function initMap(){
-    //Map Options and Initial View
-    var options = {
-        zoom: 10,
-        center: {lat:52.175664, lng:-6.585877} //Kilmore Quay, County Wexford
+            //Map Options and Initial View
+            var options = {
+                zoom: 10,
+                center: {lat:52.175664, lng:-6.585877} //Kilmore Quay, County Wexford
+            }
+            
+            //New Map
+            var map = new google.maps.Map(document.getElementById('map'), options);
+            
+            addMarker({
+                coords:{lat:52.175664, lng:-6.585877},
+                iconCustom:'/assets/images/map-icons/navigation-icon.png',
+                content:'<h2>Kilmore Quay, County Wexford</h2>'
+            });
+            
+            //Add Markers Function
+            function addMarker(props){
+            var marker = new google.maps.Marker({
+                position:props.coords,
+                map:map,
+                icon:props.iconCustom
+            });
+            
+            //Check for custom icon
+            if(props.iconCustom){
+            //Set icon image
+            marker.setIcon(props.iconCustom);
+            }
+            
+            //Check content
+            if(props.content){
+                var infoWindow = new google.maps.InfoWindow({
+                content:props.content,
+                maxWidth: 300
+            });
+            
+            //Add Listener event for InfoWindow when icon is clicked
+            marker.addListener('click', function(){
+                infoWindow.open(map, marker);
+            });
+        }
     }
-    
-    //New Map
-    var map = new google.maps.Map(document.getElementById('map'), options);
-    
-    //Add Map Custom Markers
-    var marker = new google.maps.Marker({
-        position:{lat:52.175664, lng:-6.585877},
-        map:map,
-        icon:'/assets/images/map-icons/navigation-icon.png'
-    });
 }
