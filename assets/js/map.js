8b474faeef6cd9161cf3cdc07ab90d779cc4e3a4
Marie O <mariechris.obradovic@gmail.com>
2019-01-05 18:58:40 +0000

    Add animation to marker and made array marker data

--- assets/js/map.js
+++ assets/js/map.js
@@ -4,98 +4,122 @@ var map;
 var markers = [];
 
 //Main list of all locations
-let locations = [{
-    
-}];
+let locations = [
+    {
+        country: 'Ireland',
+        type: 'Restaurant',
+        name: 'Saltee Chipper',
+        lat: 52.1729,
+        lng: -6.5897,
+        iconCustom: '.../assets/images/map-icons/restaurant-icon.png'
+    },
+    {
+        country: 'Ireland',
+        type: 'Restaurant',
+        name: 'Lobster Pot',
+        lat: 52.198706,
+        lng: -6.374228,
+        iconCustom: '.../assets/images/map-icons/restaurant-icon.png'
+    },
+    {
+        country: 'Ireland',
+        type: 'Restaurant',
+        name: 'Templars Inn',
+        lat: 52.180259,
+        lng: -6.894808,
+        iconCustom: '.../assets/images/map-icons/restaurant-icon.png'
+    },
+    {
+        country: 'Ireland',
+        type: 'Bar',
+        name: 'Kehoe\'s Pub & Parlour',
+        lat: 52.1751,
+        lng: -6.5869,
+        iconCustom: '/assets/images/map-icons/bar-icon.png'
+    },
+    {
+        country: 'Ireland',
+        type: 'Hotel',
+        name: 'Coast Kilmore Quay',
+        lat: 52.1784,
+        lng: -6.5849,
+        iconCustom: 'assets/icons/hotel.png'
+    },
+    {
+        country: 'Ireland',
+        type: 'Hotel',
+        name: 'Mill Road Farm Self Catering Cottages',
+        lat: 52.1865,
+        lng: -6.5681,
+        iconCustom: 'assets/icons/hotel.png'
+    },
+    {
+        country: 'Ireland',
+        type: 'Hotel',
+        name: 'Groveside Farm B&B',
+        lat: 52.2121,
+        lng: -6.5722,
+        iconCustom: 'assets/icons/hotel.png'
+    },
+];
 
 //Initialize Map
-function initMap(){
-            //Map Options and Initial View
-            var options = {
-                zoom: 10,
-                center: {lat:52.175664, lng:-6.585877} //Kilmore Quay, County Wexford
-            }
-            
-            //New Map
-            var map = new google.maps.Map(document.getElementById('map'), options);
-            
-            //This is the format to push custom markers in Google Maps
-            //saving this for concat or push function later to display markers.
-            //just testing if icons show. I realized I'd have to make a list
-            //instead of copying and pasting all the data. Then I have to
-            //make an array of all info then just draw the information I need.
-            //Navigation Initial Marker
-            addMarker({
-                coords:{lat:52.175664, lng:-6.585877}, //Kilmore Quay, County Wexford
-                iconCustom:'./assets/images/map-icons/navigation-icon.png',
-                content:'<h2>Kilmore Quay, County Wexford</h2>'
-            });
-            
-            //Restaurants
-            addMarker({
-                coords:{lat:52.1729, lng:-6.5897}, //Saltee Chipper
-                iconCustom:'./assets/images/map-icons/restaurant-icon.png',
-                content:'<h2>Saltee Chipper Restaurant</h2>'
-            });
-            
-            addMarker({
-                coords:{lat:52.198706, lng:-6.374228}, //Lobster Pot
-                iconCustom:'./assets/images/map-icons/restaurant-icon.png',
-                content:'<h2>Lobster Pot Restaurant</h2>'
-            });
-            
-            addMarker({
-                coords:{lat:52.180259, lng:-6.894808}, //Templars Inn
-                iconCustom:'./assets/images/map-icons/restaurant-icon.png',
-                content:'<h2>Templars Inn Restaurant</h2>'
-            });
-            
-            //Bars
-            addMarker({
-                coords:{lat:52.190840, lng:-6.839256}, //Nevilles Bar and Kitchen
-                iconCustom:'./assets/images/map-icons/bar-icon.png',
-                content:'<h2>Nevilles Bar and Kitchen</h2>'
-            });
-            addMarker({
-                coords:{lat:52.1751, lng:-6.5869}, //Meyler's Millhouse Bar & Restaurant
-                iconCustom:'./assets/images/map-icons/bar-icon.png',
-                content:'<h2>Meyler\'s Millhouse Bar & Restaurant</h2>'
-            });
-            addMarker({
-                coords:{lat:52.2101, lng:-6.3804}, //The Island Bar
-                iconCustom:'./assets/images/map-icons/bar-icon.png',
-                content:'<h2>The Island Bar</h2>'
-            });
-            
-            //Add Markers Function
-            function addMarker(props){
-            var marker = new google.maps.Marker({
-                position:props.coords,
-                map:map,
-                icon:props.iconCustom
-            });
-            
-            //Check for custom icon
-            if(props.iconCustom){
+function initMap() {
+    //Map Options and Initial View
+    var options = {
+        zoom: 10,
+        center: { lat: 52.175664, lng: -6.585877 } //Kilmore Quay, County Wexford
+    }
+
+    //New Map
+    var map = new google.maps.Map(document.getElementById('map'), options);
+
+    //Add Markers Function
+    function addMarker(props) {
+        var marker = new google.maps.Marker({
+            position: props.coords,
+            map: map,
+            icon: props.iconCustom,
+            animation: google.maps.Animation.DROP
+        });
+
+        //Check for custom icon
+        if (props.iconCustom) {
             //Set icon image
             marker.setIcon(props.iconCustom);
-            }
-            
-            //Check content
-            if(props.content){
-                var infoWindow = new google.maps.InfoWindow({
-                content:props.content,
+        }
+
+        //Check content
+        if (props.content) {
+            var infoWindow = new google.maps.InfoWindow({
+                content: props.content,
                 maxWidth: 200
             });
-            
+
             //Add Listener event for InfoWindow when icon is clicked
-            var myListener = marker.addListener('click', function(){
+            var myListener = marker.addListener('click', function() {
                 infoWindow.open(map, marker);
             });
-            
+
             //Testing this if adding time delay for execution is possible
             //I will need something similar for clearing map markers
             /*google.maps.event.removeListener(myListener);*/
+            
+            //If you click the marker, it will bounce.
+            marker.addListener('click', toggleBounce);
+        }
+        
+        //Will make the marker bounce
+        function toggleBounce() {
+            //Means if it has animation,
+            if (marker.getAnimation() !== null) {
+                //then stop its animation.
+                marker.setAnimation(null);
+            }
+            //If it is not animated, then make it bounce.
+            else {
+                marker.setAnimation(google.maps.Animation.BOUNCE);
+            }
         }
     }
 }
